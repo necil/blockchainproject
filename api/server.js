e552236f7fe1a88a26529d4c7b8b2923b94f70b3
necil <ncl.albayrak@gmail.com>
2017-04-28 18:25:59 +0300

    adde blockchain connection

--- api/server.js
+++ api/server.js
@@ -12,18 +12,20 @@ app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
 function	callf(a,b)	{
-	var	ia	=	parseInt(a)	;	
+	var	ia	=	parseInt(a)	;
 	var	ib	=	parseInt(b)	;
   return(	contract.f(ia,ib)	)
 }
 
 web3.setProvider(new	web3.providers.HttpProvider('http://localhost:8545'));
-var	abi	=	[{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],
-"name":"f","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"}]
-var	contractaddress	=	"0x0a369d1f06adca3be0ff14a5340d8186e296b8e8"	;
+var	abi	=	[{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"type":"function"},
+{"constant":false,"inputs":[{"name":"_greeting","type":"string"}],"name":"new_greeter","outputs":[],"payable":false,"type":"function"},
+{"constant":true,"inputs":[],"name":"greet","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},
+{"inputs":[{"name":"_greeting","type":"string"}],"payable":false,"type":"constructor"}]
+var	contractaddress	=	"0x565bba7fc5b638ee8230d2f78446d1245f155f7d"	;
 var	contract	=	web3.eth.contract(abi).at(contractaddress);
 if(!web3.isConnected())	{
-				console.log("not	connected");
+	console.log("not	connected");
 }
 // routes ======================================================================
 require('./routes/route.js')(app); // load our routes and pass in our app and fully configured passport
